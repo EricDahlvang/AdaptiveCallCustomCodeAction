620c924501153049599448ee771726d65c24be76
Eric Dahlvang <erdahlva@microsoft.com>
2020-11-25 15:05:08 -0800

    add project

---
+++ EncryptionActions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotFramework.Composer.CustomAction
+{
+    public class EncryptionActions
+    {
+        // The encryption key name in settings.
+        private const string EncryptionKeySettingName = "settings.encryptionKey";
+
+        // The name of the property to encrypt or decrypt. 
+        private const string DialogPropertyName = "dialog.encryptionTarget";
+
+        public async Task<DialogTurnResult> EncryptProperty(DialogContext dc, System.Object options)
+        {
+            var propertyName = dc.State.GetValue<string>(DialogPropertyName, () => string.Empty);
+            var propertyValue = dc.State.GetValue<string>(propertyName, () => string.Empty);
+
+            var key = dc.State.GetValue<string>(EncryptionKeySettingName, () => string.Empty);
+            var encrypted = Encryption.AESThenHMAC.SimpleEncryptWithPassword(propertyValue, key);
+
+            dc.State.SetValue(propertyName, encrypted);
+
+            return await dc.EndDialogAsync(options).ConfigureAwait(false);
+        }
+        
+        public async Task<DialogTurnResult> DecryptProperty(DialogContext dc, System.Object options)
+        {
+            var propertyName = dc.State.GetValue<string>(DialogPropertyName, () => string.Empty);
+            var propertyValue = dc.State.GetValue<string>(propertyName, () => string.Empty);
+
+            var key = dc.State.GetValue<string>(EncryptionKeySettingName, () => string.Empty);
+            var decrypted = Encryption.AESThenHMAC.SimpleDecryptWithPassword(propertyValue, key);
+
+            dc.State.SetValue(propertyName, decrypted);
+
+            return await dc.EndDialogAsync(options).ConfigureAwait(false);
+        }
+    }
+}
---
+++ MathCodeActions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotFramework.Composer.CustomAction
+{
+    public class MathCodeActions
+    {
+        public async Task<DialogTurnResult> Multiply(DialogContext dc, System.Object options)
+        {
+            var asJobject = options as JObject;
+
+            var v1 = dc.State.GetValue<float>("dialog.variable1", () => 0);
+            var v2 = dc.State.GetValue<float>("dialog.variable2", () => 0);
+            return await dc.EndDialogAsync(new { value = v1 * v2 }).ConfigureAwait(false);
+        }
+
+        public async Task<DialogTurnResult> Divide(DialogContext dc, System.Object options)
+        {
+            var v1 = dc.State.GetValue<float>("dialog.variable1", () => 0);
+            var v2 = dc.State.GetValue<float>("dialog.variable2", () => 0); // divide by zero?
+
+            return await dc.EndDialogAsync(new { value = v1 / v2 }).ConfigureAwait(false);
+        }
+
+        public async Task<DialogTurnResult> Add(DialogContext dc, System.Object options)
+        {
+            var v1 = dc.State.GetValue<float>("dialog.variable1", () => 0);
+            var v2 = dc.State.GetValue<float>("dialog.variable2", () => 0);
+
+            return await dc.EndDialogAsync(new { value = v1 + v2 }).ConfigureAwait(false);
+        }
+
+        public async Task<DialogTurnResult> Subtract(DialogContext dc, System.Object options)
+        {
+            var v1 = dc.State.GetValue<float>("dialog.variable1", () => 0);
+            var v2 = dc.State.GetValue<float>("dialog.variable2", () => 0);
+
+            return await dc.EndDialogAsync(new { value = v1 - v2 }).ConfigureAwait(false);
+        }
+    }
+}
